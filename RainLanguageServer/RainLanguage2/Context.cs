0230c4969fa45de8d6b60f042620561e840bb500
渴望蓝天 <1457675796@qq.com>
2024-07-11 04:45:46 +0800

    准备链接

---
+++ RainLanguageServer/RainLanguage2/Context.cs
@@ -0,0 +1,10 @@
+﻿namespace RainLanguageServer.RainLanguage2
+{
+    internal readonly struct Context(TextDocument document, AbstractSpace space, HashSet<AbstractSpace> relies, AbstractDeclaration? declaration)
+    {
+        public readonly TextDocument document = document;
+        public readonly AbstractSpace space = space;
+        public readonly HashSet<AbstractSpace> relies = relies;
+        public readonly AbstractDeclaration? declaration = declaration;
+    }
+}
--- RainLanguageServer/RainLanguage2/FileLink.cs
+++ RainLanguageServer/RainLanguage2/FileLink.cs
@@ -2,9 +2,23 @@
 {
     internal static class FileLink
     {
+        private static Type GetType(Context context, Manager manager, FileType type)
+        {
+            return default;
+        }
         public static void Link(Manager manager, AbstractLibrary library, FileSpace space)
         {
-
+            foreach (var child in space.children) Link(manager, library, child);
+            var context = new Context(space.document, space.space, space.relies, default);
+            foreach (var file in space.variables)
+            {
+                var type = GetType(context, manager, file.type);
+                var declaration = Utility.GetDeclaration(manager, library, file.visibility, DeclarationCategory.Variable);
+                var variable = new AbstractVariable(file, space.space, file.name, declaration, file.isReadonly, type);
+                if (file.isReadonly)
+                {
+                }
+            }
         }
     }
 }
